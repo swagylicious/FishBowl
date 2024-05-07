f976e73928c5e3166fc9590e0212fecf50c70863
swagylicious <jflores23@nait.ca>
2024-05-07 03:35:56 +0000

    May 6, 2024, 9:35 PM

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ let intervalId = null;
 const fishbowl = document.getElementById('fishbowl');
 const fish = document.getElementById('fish');
 const tap    = document.getElementById('tap');
+
+const emptyFishbowl = () => {
