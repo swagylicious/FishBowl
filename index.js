60392bac840c0b9d9402e11643d7ca0e3d751175
swagylicious <jflores23@nait.ca>
2024-05-07 03:36:13 +0000

    May 6, 2024, 9:36 PM

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ const fishbowl = document.getElementById('fishbowl');
 const fish = document.getElementById('fish');
 const tap    = document.getElementById('tap');
 
-const emptyFishbowl = () => {
+const emptyFishbowl = () => setInterval(() => {
+  if (fill > 0) {
+    fill--;
+    fishbowl.style.background = `linear-gradient(to top, #f1c40f ${fill}%, #3498db ${fill}%)`;
+  } else {
+    clearInterval(intervalId);
+    tap.style.display = 'block';
+  }
+}, 1000);
